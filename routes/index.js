e2ff4c76c49f032e10a80172a3b850528d5c6c31
lei-clearsky <sportzhulei@gmail.com>
2015-02-24 21:15:55 -0500

    finished everything except for creating new user and tweet

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 //var tweetBank = require('../tweetBank');
 var tweetSql = require('../models');
+//require('../models');
 router.get('/', function (req, res) {
   tweetSql.Tweet.findAll({include: [tweetSql.User]}).then(function(tweets){
     //console.log(tweets);
@@ -19,32 +20,104 @@ router.get('/', function (req, res) {
 router.get('/users/:name', function(req, res) {
   var name = req.params.name;
   //var list = tweetBank.find( {name: name} );
-  tweetSql.User.findAll({where:{name: name}, include: [tweetSql.Tweet]}).then(function(user){
-    console.log(JSON.stringify(user));
-   res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: user[0].name, tweets: user[0].Tweets, showForm: true } );
+  // tweetSql.User.findAll({where:{name: name}, include: [tweetSql.Tweet]}).then(function(user){
+  //   console.log(JSON.stringify(user));
+  //  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: user[0].name, tweets: user[0].Tweets, showForm: true } );
  
+  // });
+  tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where:{name:name}}]}).complete(function(err, tweets){
+    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets, showForm: true } );
   });
 });
 
 router.get('/users/:name/tweets/:id', function(req, res) {
   var name = req.params.name;
   var id = parseInt(req.params.id);
-  var list = tweetBank.find( {id: id} );
-  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
+  // var list = tweetBank.find( {id: id} );
+  // res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
+
+  tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where: {id: id}}]}).complete(function(err, tweets){
+    res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets} );
+  });
 });
 
 router.post('/submit', function(req, res) {
   var name = req.body.name;
   var text = req.body.text;
-  tweetBank.add(name, text);
+  // tweetBank.add(name, text);
 
-  // test
-  var tweets = tweetBank.list();
-  var newTweet = tweets[tweets.length - 1];
-  io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: newTweet.name, text: newTweet.text});
-  // io.sockets.emit('new_tweet', {heading: 'New Tweet', name: name, text: text});
+  // var tweets = tweetBank.list();
+  // var newTweet = tweets[tweets.length - 1];
+  // io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: newTweet.name, text: newTweet.text});
 
-  res.redirect('/');
+  // res.redirect('/');
+
+  // tweetSql.Tweet
+  // .findAll({include: [tweetSql.User]})
+  // .build({ tweet: text, name: name })
+  // .save()
+  // .then(function(newTweet) {
+  //   io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: newTweet.name, text: newTweet.tweet});
+  //   res.redirect('/');
+  // }).catch(function(error) {
+  //   // Ooops, do some error-handling
+
+  // })
+  var existingUserID = -1;
+
+  tweetSql.User.findAll({where:{name: name}})
+  .complete(function(err, userName){
+    if (err) {
+      tweetSql.User.create({ UserId: autoincrement, name: name });
+      // find new user id
+      var newUserId = ...
+      // create
+      tweetSql.Tweet
+      .create({ UserId: newUserId, tweet: text })
+      .then(function(newTweet) {
+        // tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where: {id: id}}]}).complete(function(err, tweets){
+        //   res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets} );
+        // });
+        io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: name, text: newTweet.tweet});
+        res.redirect('/');
+      }).catch(function(error) {
+      // Ooops, do some error-handling
+
+      });
+    } else {
+      existingUserID = userName.shift().dataValues.id;
+      // console.log(existingUserID);
+      tweetSql.Tweet
+      .create({ UserId: existingUserID, tweet: text })
+      .then(function(newTweet) {
+        // tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where: {id: id}}]}).complete(function(err, tweets){
+        //   res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets} );
+        // });
+        io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: name, text: newTweet.tweet});
+        res.redirect('/');
+      }).catch(function(error) {
+      // Ooops, do some error-handling
+
+      });
+    }
+
+  });
+
+
+  
+
+
+
+  // .build({ tweet: text, name: name })
+  // .save()
+  // .then(function(newTweet) {
+  //   io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: newTweet.name, text: newTweet.tweet});
+  //   res.redirect('/');
+  // }).catch(function(error) {
+  //   // Ooops, do some error-handling
+
+  // })
+
 });
 
 return router;
