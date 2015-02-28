58d3a33c5d4ba693b89e2c7820fb7e1db9696bf6
lei-clearsky <sportzhulei@gmail.com>
2015-02-28 08:52:55 -0500

    add twitter-sql ws review

--- routes/index.js
+++ routes/index.js
@@ -1,96 +1,31 @@
 var express = require('express');
 var tweetSql = require('../models');
-// var models = require('../models'),
-//     Tweet = models.Tweet,
-//     User = models.User;
-
 
 module.exports = function (io) {
   var router = express.Router();
-  // could use one line instead: var router = require('express').Router();
-  // review
-  // router.get('/', function(req, res){
-  //   Tweet
-  //   .findAll()
-  //   .success(function (tweets) {
-  //     console.log('tweets', tweets.map(function (tweet) {
-  //       var vals = tweet.get();
-  //       vals.User = tweet.User.get();
-  //       return vals;
-  //     }));
-  //     res.render('index', {
-  //       tweets: tweets,
-  //       showForm: true
-  //     });
-  //   });
-  // });
+
   router.get('/', function (req, res) {
     tweetSql.Tweet.findAll({include: [tweetSql.User]}).then(function(tweets){
       console.log(JSON.stringify(tweets));
       //var tweetsJSON = JSON.stringify(tweets);
       res.render('index', {title: 'Twitter.js', tweets: tweets, showForm: true});
     });
-    //var tweets = tweetBank.list();
-    //res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
   });
 
   router.get('/users/:name', function(req, res) {
     var name = req.params.name;
 
-  //   User
-  //   .find({where: {name: req.params.name}})
-  //   .success(function (user) {
-  //     user
-  //     .getTweets()
-  //     .success(function (tweets) {
-  //       var tweetVals = tweets.map(function (tweet) {
-  //         var vals = tweet.get();
-  //         vals.User = user;
-  //         return vals;
-  //       });
-  //       console.log('tweets', tweetVals);
-  //       res.render( 'index', { 
-  //         title: 'Twitter.js - Posts by ' + req.params.name, 
-  //         name: req.params.name, 
-  //         tweets: tweetVals, 
-  //         showForm: true 
-  //       } );
-  //     });
-
-  //   });
-
-  // });  
-
   router.get('/users/:name', function(req, res) {
     var name = req.params.name;
-    //var list = tweetBank.find( {name: name} );
-    // tweetSql.User.findAll({where:{name: name}, include: [tweetSql.Tweet]}).then(function(user){
-    //   console.log(JSON.stringify(user));
-    //  res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: user[0].name, tweets: user[0].Tweets, showForm: true } );
-   
-    // });
+
     tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where:{name:name}}]}).complete(function(err, tweets){
       res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets, showForm: true } );
     });
   });
 
-  // router.get('/users/:name/tweets/:id', function(req, res) {
-  //   req.params.id = parseInt(req.params.id);
-  //   Tweet
-  //   .findAll({include: [User], 
-  //             where: {id: req.params.id}})
-  //   .success(function(tweets) {
-  //     res.render('index', {
-  //       tweets: tweets
-  //     })
-  //   });
-  // }); 
-
   router.get('/users/:name/tweets/:id', function(req, res) {
     var name = req.params.name;
     var id = parseInt(req.params.id);
-    // var list = tweetBank.find( {id: id} );
-    // res.render( 'index', { title: 'Twitter.js - Posts by ' + name, tweets: list } );
 
     tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where: {id: id}}]}).complete(function(err, tweets){
       res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets} );
@@ -146,9 +81,7 @@ module.exports = function (io) {
         tweetSql.Tweet
         .create({ UserId: existingUserID, tweet: text })
         .then(function(newTweet) {
-          // tweetSql.Tweet.findAll({include: [{model: tweetSql.User, where: {id: id}}]}).complete(function(err, tweets){
-          //   res.render( 'index', { title: 'Twitter.js - Posts by ' + name, name: name, tweets: tweets} );
-          // });
+
           io.sockets.emit('new_tweet', {heading: 'New Tweet', id: newTweet.id, name: name, text: newTweet.tweet});
           res.redirect('/');
         }).catch(function(error) {
@@ -163,3 +96,126 @@ module.exports = function (io) {
 return router;
 
 };
+
+// reviews
+// var express = require('express')
+// var tweetBank = require('../tweetBank');
+// var models = require('../models'),
+//   Tweet = models.Tweet,
+//   User = models.User;
+
+// module.exports = function(io) {
+//   var router = express.Router()
+
+//   router.get('/', function(req, res) {
+//     // find all tweets
+//     // render them
+//     Tweet
+//     .findAll({include: [User]})
+//     .success(function (tweets) {
+//       console.log('tweets', tweets.map(function (tweet) {
+//         var vals = tweet.get();
+//         vals.User = tweet.User.get();
+//         return vals;
+//       }));
+//       res.render('index', {
+//         tweets: tweets,
+//         showForm: true
+//       });
+//     });
+
+//   })
+
+//   router.get('/users/:name', function(req, res) {
+//     User
+//     .find({where: {name: req.params.name}})
+//     .success(function (user) {
+//       user
+//       .getTweets()
+//       .success(function (tweets) {
+//         var tweetVals = tweets.map(function (tweet) {
+//           var vals = tweet.get();
+//           vals.User = user;
+//           return vals;
+//         });
+//         console.log('tweets', tweetVals);
+//         res.render('index', {
+//           tweets: tweetVals,
+//           formName: req.params.name,
+//           showForm: true
+//         })
+//       });
+//     });
+//   })
+
+//   router.get('/users/:name/tweets/:id', function(req, res){
+//     req.params.id = parseInt(req.params.id);
+//     Tweet
+//     .find({include: [User]}, req.params.id)
+//     .success(function (tweet) {
+//       res.render('index', {
+//         tweets: [tweet]
+//       })
+//     });
+//   })
+
+//   router.post('/submit', function(req, res) {
+//     // see if user exists
+//     // if it does
+//     //  create the new tweet
+//     //  with that user's id
+//     // otherwise
+//     //  create a new user
+//     //  THEN create the new tweet
+//     //  with that user's id
+//     // // --- NAIVE APPROACH ---
+//     // User
+//     // .find({where: {name: req.body.name}})
+//     // .success(function (foundUser) {
+//     //  if (foundUser === null) {
+//     //    User
+//     //    .create({name: req.body.name})
+//     //    .success(function (createdUser) {
+//     //      Tweet
+//     //      .create({tweet: req.body.text, UserId: createdUser.id})
+//     //      .success(function (tweet) {
+//     //        tweet.User = createdUser;
+//     //        io.sockets.emit('new_tweet', tweet);
+//     //        res.redirect(req.body.redirectUrl);
+//     //      });
+//     //    });
+//     //  } else {
+//     //    Tweet
+//     //    .create({tweet: req.body.text, UserId: foundUser.id})
+//     //    .success(function (tweet) {
+//     //      tweet.User = foundUser;
+//     //      io.sockets.emit('new_tweet', tweet);
+//     //      res.redirect(req.body.redirectUrl);
+//     //    });
+//     //  }
+//     // });
+//     // --- FIND OR CREATE APPROACH
+//     User
+//     .findOrCreate({
+//       where: {name: req.body.name},
+//       defaults: {name: req.body.name}
+//     })
+//     .success(function (arr) {
+//       var user = arr[0];
+//       Tweet
+//       .create({tweet: req.body.text, UserId: user.id})
+//       .success(function (tweet) {
+//         // also fixed sockets
+//         io.sockets.emit('new_tweet', {
+//           tweet: tweet.tweet,
+//           User: {
+//             name: user.name
+//           }
+//         });
+//         res.redirect(req.body.redirectUrl);
+//       });
+//     });
+//   })
+
+//   return router
+// }
